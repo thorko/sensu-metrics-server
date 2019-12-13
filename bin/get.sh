2c7b14ba9517eae5bd2af302c9e161457db92893
Thorsten Müller <t.mueller@denic.de>
2019-12-13 18:10:48 +0100

    fix path

--- bin/get.sh
+++ bin/get.sh
@@ -2,12 +2,12 @@
 
 url=$1
 
-out=$(./curl -s $1)
+out=$(curl -s $1)
 
 d=$(date +%s)
 
 IFS=$'\n'
 for line in $out; do
-	a=$(echo $line | ./sed 's/{/,/g' | ./sed 's/[}"]//g')
-	echo $a | ./awk -v date="$d" '{print $1" value="$2" "date}'
+	a=$(echo $line | sed 's/{/,/g' | sed 's/[}"]//g')
+	echo $a | awk -v date="$d" '{print $1" value="$2" "date}'
 done
